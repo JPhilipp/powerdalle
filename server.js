23a00656e89889b88cf57076eba78df6b497e776
Philipp Lenssen <philipp.lenssen@gmail.com>
2023-11-07 13:38:18 +0100

    Rename api key variable

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post('/generate-image', async (req, res) => {
     const response = await fetch('https://api.openai.com/v1/images/generations', {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${process.env.DALLE_API_KEY}`,
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
