d027df0cd1ba353d21a8a58ce17e30b1aa83edb6
Philipp Lenssen <philipp.lenssen@gmail.com>
2023-11-12 19:41:46 +0100

    Add start.bat

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+where nodemon >nul 2>&1
+if %ERRORLEVEL% equ 0 (
+    start cmd /c nodemon server.js
+) else (
+    start cmd /c node server.js
+)
+timeout /t 1 /nobreak
+start http://localhost:3000/
